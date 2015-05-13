39b166a39a7134af6fe688a9d3c4c89764761d3a
danlyg <gih.daniel@gmail.com>
2015-05-12 18:45:34 -0700

    added bonus to solution

--- homework/index.js
+++ homework/index.js
@@ -1,15 +1,14 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
-
+var RString = require("randomstring");
 
 var app = express();
 var views = path.join(process.cwd(), "views"); //look this up
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-
-var urls = ["http://www.google.com", "http://www.reddit.com", "http://www.facebook.com"]
+var urls = {};
 
 
 app.get('/', function (req, res) {
@@ -20,26 +19,20 @@ app.get('/', function (req, res) {
 app.get("/urls", function (req, res) {
 	var urlsText = urls.join(", ");
 	res.send(urlsText);
-})
+});
 
 app.post("/urls", function (req, res) {
 	console.log(req.body.url);
 	var newUrl = req.body.url;
-	urls.push(newUrl.link);
-	res.send('url received ' + newUrl.link)
+	var randomString = RString.generate();
+	urls[randomString] = newUrl.link;
+	res.send('URL shortened, GOTO localhost:3000/urls/' + randomString);
 });
 
 app.get("/urls/:index", function (req, res) {
 	var url = urls[req.params.index];
 	res.redirect(url);
 });
-
-
-
-
-
-
-
 
 app.listen(3000, function (req, res) {
 	console.log("working!!")
